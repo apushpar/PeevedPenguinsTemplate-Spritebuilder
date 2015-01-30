6a25fe27f1370115634069c7024ac5d41c3fc423
Akshay <akshay.pushparaja@gmail.com>
2015-01-30 13:29:36 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,24 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Akshay Pushparaja on 1/30/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+-(id)init{
+    
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Seal created.");
+    }
+    
+    return self;
+}
+
+@end
