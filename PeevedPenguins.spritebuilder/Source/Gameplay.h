89517df54beae08155cbafcce19c8542ab0114ef
Akshay <akshay.pushparaja@gmail.com>
2015-01-30 17:24:46 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Akshay Pushparaja on 1/30/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -0,0 +1,35 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Akshay Pushparaja on 1/30/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay{
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+
+-(void)didLoadFromCCB {
+    self.userInteractionEnabled = TRUE;
+}
+
+-(void) touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
+    [self launchPenguin];
+}
+
+-(void) launchPenguin {
+    CCNode* penguin = [CCBReader load:@"Penguin"];
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
+    
+    [_physicsNode addChild:penguin];
+    
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [penguin.physicsBody applyForce:force];
+}
+
+@end
